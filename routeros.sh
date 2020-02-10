26eecd1aca0b26f8e14e5d786abf82c449eaf7f7
GeekTR <geektheripper@gmail.com>
2020-02-11 02:17:37 +0800

    fix wrong ip address

--- routeros.sh
+++ routeros.sh
@@ -13,8 +13,8 @@ add chain=forward action=fasttrack-connection
 
 /ip firewall nat
 add action=masquerade chain=srcnat out-interface=ether1
-add action=dst-nat chain=dstnat dst-port=80  in-interface=ether1 protocol=tcp to-addresses=10.2.1.10 to-ports=80
-add action=dst-nat chain=dstnat dst-port=443 in-interface=ether1 protocol=tcp to-addresses=10.2.1.10 to-ports=443
-add action=dst-nat chain=dstnat dst-port=22  in-interface=ether1 protocol=tcp to-addresses=10.2.1.10 to-ports=22
+add action=dst-nat chain=dstnat dst-port=80  in-interface=ether1 protocol=tcp to-addresses=10.0.0.4 to-ports=80
+add action=dst-nat chain=dstnat dst-port=443 in-interface=ether1 protocol=tcp to-addresses=10.0.0.4 to-ports=443
+add action=dst-nat chain=dstnat dst-port=22  in-interface=ether1 protocol=tcp to-addresses=10.0.0.4 to-ports=22
 
 /ip service set ssh port=2222
